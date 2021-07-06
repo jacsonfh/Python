ac7ac9336ecba7bfa4828ee0227e50b475c74f49
jacsonfh <jacsonfh@gmail.com>
2021-07-06 15:10:23 -0300

    add ex1.py

---
+++ ex1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Modelo de aquivo para programa."""
+
+"""Module documentation goes here
+   and here
+   and ...
+"""
+
+__author__ = "Jacson F. Heiderscheidt"
+__copyright__ = "Copyright 2021, Allpy Project"
+__credits__ = ["JFH"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "JFH"
+__email__ = "jacsonfh@gmail.com"
+__status__ = "Test"
+
+
