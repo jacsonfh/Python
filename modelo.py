e3e0491b1549773ad03e5b2c55dd9fb03693c47e
jacsonfh <jacsonfh@gmail.com>
2021-07-06 15:12:17 -0300

    v1.0

--- modelo.py
+++ modelo.py
@@ -3,11 +3,16 @@
 
 """Modelo de aquivo para programa."""
 
+"""Module documentation goes here
+   and here
+   and ...
+"""
+
 __author__ = "Jacson F. Heiderscheidt"
 __copyright__ = "Copyright 2021, Allpy Project"
 __credits__ = ["JFH"]
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "0.0.1"
 __maintainer__ = "JFH"
 __email__ = "jacsonfh@gmail.com"
 __status__ = "Test"
