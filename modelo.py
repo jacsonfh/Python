e3e92f8e117facc93f67afc3a5d7ea43fd5d2ae7
jacsonfh <jacsonfh@gmail.com>
2021-07-06 09:54:22 -0300

    atualiza modelo.py

--- modelo.py
+++ modelo.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """Modelo de aquivo para programa."""
 
-__author__ = "Rob Knight, Gavin Huttley, and Peter Maxwell"
-__copyright__ = "Copyright 2007, The Cogent Project"
-__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
-                    "Matthew Wakefield"]
+__author__ = "Jacson F. Heiderscheidt"
+__copyright__ = "Copyright 2021, Allpy Project"
+__credits__ = ["JFH"]
 __license__ = "GPL"
 __version__ = "1.0.1"
-__maintainer__ = "Rob Knight"
-__email__ = "rob@spot.colorado.edu"
-__status__ = "Production"
+__maintainer__ = "JFH"
+__email__ = "jacsonfh@gmail.com"
+__status__ = "Test"
+
+
