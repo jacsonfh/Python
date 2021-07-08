ed5fc50a2f839013bc457cfd7765e43d13bfe8ec
jacsonfh <jacsonfh@gmail.com>
2021-07-08 19:31:02 -0300

    update 3

--- basic/ex003_operadores_primitivos.py
+++
@@ -1,3 +0,0 @@
-valor1 = int(input('Digite o valor1:'))
-valor2 = int(input('Digite o valor2:'))
-print('A soma dos valores é', valor1 + valor2)
---
+++ basic/ex003e4_operadores_primitivos.py
@@ -0,0 +1,18 @@
+#valor1 = int(input('Digite o valor1:'))
+#valor2 = int(input('Digite o valor2:'))
+#print('A soma dos valores é', valor1 + valor2)
+#soma = valor1 + valor2
+#print('A soma entre {} e {} é {}'.format(valor1, valor2, soma))
+
+""" Os principais tipos pritivos são:
+int = número inteiro.
+float = número de ponto flutuante ou reais.
+bool = expressao boleana.
+str = texto """
+qqc = input('Digite quaquer coisa:')
+print('O tipo primitivo é:', type(qqc))
+print(qqc.isalpha(), 'é Alpha.')
+print(qqc.isspace(), 'é Espaço.')
+print(qqc.isupper(), 'é maiúsculo.')
+print(qqc.islower(), 'é minúsculo.')
+print(qqc.isnumeric(), 'é número.')
