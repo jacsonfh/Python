02fcf17417eba0f0e9c3886f6bbd008902153b0c
jacsonfh <jacsonfh@gmail.com>
2021-07-08 18:11:01 -0300

    ex 2

---
+++ basic/inputs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""input."""
+
+
+__author__ = "Jacson F. Heiderscheidt"
+__copyright__ = "Copyright 2021, Allpy Project"
+__credits__ = ["JFH"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "JFH"
+__email__ = "jacsonfh@gmail.com"
+__status__ = "input"
+
+nome = input('Qual o seu nome? ')
+print('Bem vindo ' + nome)
