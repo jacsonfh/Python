59ddd6d8f9eacc5e5f07f422d55f9230d78f7418
jacsonfh <jacsonfh@gmail.com>
2021-07-29 12:30:20 -0300

    Update gui.py
    Alinhamentos

--- gui/gui.py
+++ gui/gui.py
@@ -10,16 +10,19 @@ def mostraDiscos(recebe1):
 
 frmPrincipal = tkinter.Tk()
 frmPrincipal.title("Allpy")
-frmPrincipal.minsize("800","600")
+frmPrincipal.minsize("400","300")
 
 lblMenu = Label(frmPrincipal, text="Menu")
-lblMenu.pack(side=LEFT)
+lblMenu.grid(row=1, column=1)
+#lblMenu.pack(side=LEFT)
 
 txtMenu = StringVar()
 txtMenu = Entry(frmPrincipal,textvariable=txtMenu)
-txtMenu.pack(side=LEFT)
+txtMenu.grid(row=2, column=1)
+#txtMenu.pack(side=LEFT)
 
 cmdOk = Button(frmPrincipal, text="Pesquisar", command=lambda: mostraDiscos(txtMenu.get()))
-cmdOk.pack(side=LEFT)
+cmdOk.grid(row=3, column=1)
+#cmdOk.pack(side=LEFT)
 
 frmPrincipal.mainloop()
