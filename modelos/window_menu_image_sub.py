a9bed591f9722b49c998cb129f2cbdb991102725
jacsonfh <jacsonfh@users.noreply.github.com>
2021-08-10 09:03:57 -0300

    Update window_menu_image_sub.py

--- modelos/window_menu_image_sub.py
+++ modelos/window_menu_image_sub.py
@@ -32,3 +32,23 @@ submenu2.add_command(label="Option 2.2")
 imgvar = PhotoImage(file='eye.gif')
 button = Button(root, image=imgvar)
 button.grid(row=0, column=2)
+
+
+"""
+import tkinter as tk
+
+from tkinter import *
+
+
+your_app_name = Tk()
+menubar = Menu(your_app_name)
+
+file = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Command", menu=file_menu)
+icon = PhotoImage(file='your_file.png') # or: (file='./your_file.png')
+file_menu.add_command(label='New', image=icon, compound='left', command=some_function)
+
+your_app_name.config(menu=menubar)
+your_app_name.mainloop()
+
+"""
